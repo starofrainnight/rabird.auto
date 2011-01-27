ca56bc11777cd2ae8610293c0d4dd36d04972700
Hong-She Liang <starofrainnight@gmail.com>
2011-01-27 16:30:46 +0000

    Removed unused rabird_base.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import sys
 
-import rabird_base as base
-import rabird_windows_api as windows_api
+if sys.platform == "win32" :
+	import rabird_windows_fix
+	import rabird_windows_api as windows_api
--- rabird_base.py
+++
@@ -1,6 +0,0 @@
-## base library import list
-#
-import sys
-
-if sys.platform == "win32" :
-	import rabird_windows_fix
