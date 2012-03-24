146e5af26b437d044455a7963562e120abe9d954
Hong-She Liang <starofrainnight@gmail.com>
2012-03-24 12:38:28 +0000

    Fixed errors that report by distutils depends by jaraco.windows.filesystem. Seems that some method has been removed from string module in python3, lead to distutils failed to do some operations.

--- rabird/__init__.py
+++ rabird/__init__.py
@@ -2,8 +2,23 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
-import sys
 
+import sys
+import string
+
+def __string_find(s, *args):
+	return s.find(*args)
+
+#
+# Fixed errors that report by distutils depends by jaraco.windows.filesystem 
+# when import rabird library : 
+#
+# File "D:\sb\python\lib\distutils\util.py", line 43, in get_platform
+# AttributeError: 'module' object has no attribute 'find'
+#
+if not hasattr(string, 'find'):
+	string.find = __string_find
+		
 if sys.platform == "win32" :
 	if sys.version_info.major <= 2 :
 		import rabird.windows_fix
