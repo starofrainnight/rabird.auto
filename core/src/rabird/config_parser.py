cf7dbb8af908d91a2c477aaef6c7e29ba33ae4d0
Hong-She Liang <starofrainnight@gmail.com>
2013-05-10 09:06:52 +0000

    Added config_parser module that provide ability that silent add section if there do not have a section.

---
+++ core/src/rabird/config_parser.py
@@ -0,0 +1,14 @@
+'''
+Created on 2013-5-10
+
+@author: "HongShe Liang <starofrainnight@gmail.com>"
+'''
+
+from six.moves import configparser
+
+class config_parser_t(configparser.ConfigParser):
+	def set(self, section, option, value):
+		if not self.has_section(section):
+			self.add_section(section)
+		
+		super(config_parser_t, self).set(section, option, str(value))
