82591b7b395b03104fa3a6e518e5eb6fa17c7910
Hong-She Liang <starofrainnight@gmail.com>
2013-07-06 16:38:47 +0000

    Fixed all file names with python name standard ( again too ! )

--- core/src/rabird/__init__.py
+++ core/src/rabird/__init__.py
@@ -5,7 +5,7 @@ __import__('pkg_resources').declare_namespace(__name__)
 
 import sys
 
-version_info = (0, 0, 0, 50)
+version_info = (0, 0, 0, 51)
 __version__ = '.'.join(map(str, version_info))
 
 try:
