fe15f747d4dfe7cec68f1111459d95ce0630395b
Hong-She Liang <starofrainnight@gmail.com>
2013-08-14 02:58:43 +0000

    Fixed can not import any modules from rabird.automation Added window module to handle all windows find/check operations.

--- automation/src/rabird/automation/__init__.py
+++ automation/src/rabird/automation/__init__.py
@@ -3,5 +3,9 @@
 
 import sys
 
-version_info = (0, 0, 0, 1)
+version_info = (0, 0, 0, 2)
 __version__ = '.'.join(map(str, version_info))
+
+import mouse
+import keyboard
+import window
---
+++ automation/src/rabird/automation/window.py
@@ -0,0 +1,104 @@
+'''
+Created on 2013-8-14
+
+@author: "HongShe Liang <starofrainnight@gmail.com>"
+'''
+
+# -*- coding: UTF-8 -*-
+
+#--IMPORT_ALL_FROM_FUTURE--#
+
+import win32gui
+import pywintypes
+
+# WindowTextMatchMode
+(
+WTMM_COMPLETE, ##< Complete / Slow mode (default)
+WTMM_QUICK, ##< Quick mode 
+) = range(0, 2)
+
+# WindowTitleMatchMode
+(
+WTMM2_FROM_START, ##< Match the title from the start (default)
+WTMM2_ANY, ##< Match any substring in the title
+WTMM2_EXACT, ##< Exact title match
+) = range(0, 3)
+
+# If you want to ignore case during search window title, just mask this
+# flag with option value.
+WTMM2_IGNORE_CASE_FLAG = 256
+
+__options = dict()
+__options['WindowTextMatchMode'] = WTMM_COMPLETE
+__options['WindowTitleMatchMode'] = WTMM2_FROM_START
+
+def __is_title_macth(hwnd, title):
+	target_title = win32gui.GetWindowText(hwnd)
+	title_match_option = get_option('WindowTitleMatchMode')
+	
+	if title_match_option & WTMM2_IGNORE_CASE_FLAG:
+		target_title = target_title.lower()
+		title = title.lower()
+	
+	# Extrac only the option value, excluded all flags	
+	title_match_option = title_match_option & 0xff
+	
+	print '"{}" : "{}"'.format(target_title, title)
+	
+	if title_match_option == WTMM2_FROM_START:
+		return target_title.startswith(title)
+	elif title_match_option == WTMM2_ANY:
+		return (title in target_title)
+	elif title_match_option == WTMM2_EXACT:
+		return (target_title == title)
+		
+	return False
+	
+def set_option(option_name, option_value):
+	global __options
+	
+	if option_name not in __options:
+		raise KeyError()
+		
+	__options[option_name] = option_value
+	
+	
+def get_option(option_name):
+	global __options
+	
+	if option_name not in __options:
+		raise KeyError()
+		
+	return __options[option_name]
+	
+def get_list():
+	windows = []
+	def enum_window_callback(hwnd, extra):
+		windows.append(hwnd)
+		return True
+	
+	win32gui.EnumWindows(enum_window_callback, None)
+	
+	return windows
+	
+def exists(title=None):
+	result = []
+	
+	if title is None:
+		return True;
+	
+	def enum_window_callback(hwnd, result):
+		if __is_title_macth(hwnd, title):
+			result.append(hwnd)
+			return False # Break EnumWindows() process 
+	
+	try:
+		win32gui.EnumWindows(enum_window_callback, result)
+	except pywintypes.error as e:
+		if 0== e.winerror:
+			# No errors, just function break from EnumWindows()
+			pass
+		else:
+			raise e
+	
+	return len(result) > 0	
