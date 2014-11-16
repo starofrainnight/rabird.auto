7b1c50b8e91680777776fec9dff1bd539b081464
Hong-She Liang <starofrainnight@gmail.com>
2014-11-16 14:06:05 +0000

    Moved parent's __init__() at the beginning of __init__() Implemented TinyMCE editor, now we could accept TinyMCE editor control!

--- automation/src/rabird/automation/selenium/ui.py
+++ automation/src/rabird/automation/selenium/ui.py
@@ -28,13 +28,13 @@ class BaseEditor(object):
 
 class Input(BaseEditor):
     def __init__(self, webelement):
+        super(Input, self).__init__(webelement)
+        
         if webelement.tag_name.lower() != "input":
             raise UnexpectedTagNameException("Input only works on <input> "
                                              "elements, not on <%s>" %
                                              webelement.tag_name)
             
-        super(Input, self).__init__(webelement)
-        
     @property
     def text(self):
         script = "return arguments[0].value;"
@@ -43,18 +43,18 @@ class Input(BaseEditor):
     @text.setter
     def text(self, value):
         value = utilities.js_string_encode(value)
-        script= "return arguments[0].value = '%s';" % value
+        script= "arguments[0].value = '%s';" % value
         self.element._parent.execute_script(script, self.element)
         
 class TextArea(BaseEditor):
     def __init__(self, webelement):
+        super(TextArea, self).__init__(webelement)
+        
         if webelement.tag_name.lower() != "textarea":
             raise UnexpectedTagNameException("TextArea only works on <textarea> "
                                              "elements, not on <%s>" % 
                                              webelement.tag_name)
             
-        super(TextArea, self).__init__(webelement)
-        
     @property
     def text(self):
         script = "return arguments[0].value;"
@@ -63,32 +63,62 @@ class TextArea(BaseEditor):
     @text.setter
     def text(self, value):
         value = utilities.js_string_encode(value)
-        script= "return arguments[0].value = '%s';" % value
+        script= "arguments[0].value = '%s';" % value
         self.element._parent.execute_script(script, self.element)
         
-def Editor(webelement):
-    """
-    Constructor. A check is made that the given element is, indeed, a 
-    INPUT/TEXTAREA tag. If it is not, then an UnexpectedTagNameException 
-    is thrown.
-
-    :Args:
-     - webelement - element INPUT/TEXTAREA element to wrap
+class TinyMCE(BaseEditor):
+    def __init__(self, webelement):
+        super(TinyMCE, self).__init__(webelement)
+        
+        if (webelement.tag_name.lower() != "textarea"): 
+            raise UnexpectedTagNameException("TinyMCE only works on <textarea> "
+                                             "elements, not on <%s>" % 
+                                             webelement.tag_name)
+        editors = self.get_editors()
+        id_value = webelement.get_attribute("id")
+        if id_value is None:
+            raise UnexpectedTagNameException("Textarea without 'id' attribute!")
+        
+        for aeditor in editors:
+            if aeditor == id_value:
+                break
+            
+        if aeditor != id_value:
+            raise UnexpectedTagNameException("This textarea is not an TinyMCE editor !")
+        
+    def get_editors(self):
+        script = """
+        var editors = new Array()
+        for (i in tinymce.editors) {
+            editors[i] = tinymce.editors[i].id
+        }
+        return editors;
+        """
+        return self.element._parent.execute_script(script)
+        
+    @property
+    def text(self):
+        script = "return tinymce.get('%s').getContent();" % self.element.get_attribute("id")
+        return self.element._parent.execute_script(script)
     
-    Example:
-        from selenium.webdriver.support.ui import Select \n
-        Select(driver.find_element_by_tag_name("select")).select_by_index(2)
-    """
-    accepted_classes = [Input, TextArea]
+    @text.setter
+    def text(self, value):
+        value = utilities.js_string_encode(value)
+        script= "tinymce.get('%s').setContent('%s', {format: 'raw'});" % (self.element.get_attribute("id"), value)
+        self.element._parent.execute_script(script)
+        
+def Editor(webelement):
+    # TinyMCE must before TextArea, because TinyMCE also a TextArea!
+    accepted_classes = [Input, TinyMCE, TextArea]
     result = None
     for aclass in accepted_classes:
         try:
             result = aclass(webelement)
         except UnexpectedTagNameException:
             continue
-        
+
         break
-             
+    
     if result is None:
         raise UnexpectedTagNameException("Not acceptable tag as Editor !")
         
