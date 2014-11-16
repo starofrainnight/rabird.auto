b76f1c0a17b24ee438e9847809e6458d71694998
Hong-She Liang <starofrainnight@gmail.com>
2014-11-16 07:53:56 +0000

    Removed find_elements_by_attr(), it could be replaced by find_element_by_xpath()! Removed wait_element(), we should have better implementation The action_chains seems take no effect, replaced by javascript directly.

--- automation/src/rabird/automation/selenium/__init__.py
+++ automation/src/rabird/automation/selenium/__init__.py
@@ -3,12 +3,11 @@
 
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.command import Command 
+from selenium.webdriver.remote.webdriver import WebDriver
 from . import webelement 
 
 def monkey_patch():
     WebElement.set_attribute = webelement.set_attribute
-    WebElement.find_elements_by_attr = webelement.find_elements_by_attr
-    WebElement.wait_element = webelement.wait_element    
     WebElement.force_focus = webelement.force_focus    
     WebElement.force_click = webelement.force_click
     
--- automation/src/rabird/automation/selenium/webelement.py
+++ automation/src/rabird/automation/selenium/webelement.py
@@ -18,66 +18,10 @@ def set_attribute(self, name, value):
     value = value.replace("\t", r"\t")  
     script = "arguments[0].setAttribute('%s', '%s')"  % (name, value)
     self._parent.execute_script(script, self)
-        
-def find_elements_by_attr(self, context, attributes, max_count=-1):
-    """
-    Fixed find_elements() of webelement that could not find multiply-class controls
-    
-    Invalid even methods that provided below references:
-    
-    http://stackoverflow.com/questions/15699900/compound-class-names-are-not-supported-error-in-webdriver
-    http://stackoverflow.com/questions/10658907/selenium-python-find-element-by-class-name-stopped-working-from-v-2-2-to-2-21
-    
-    @arg context tag name or elements
-    """
-    
-    result = []
-    
-    if isinstance(context, str) or isinstance(context, unicode):
-        elements = self.find_elements_by_tag_name(context)
-    else:
-        elements = context
-        
-    for element in elements:
-        is_matched = True
-        for k, v in attributes.iteritems():
-            if element.get_attribute(k) != v:
-                is_matched = False
-                break
-                
-        if not is_matched:
-            continue
-            
-        result.append(element)
-        if (max_count > 0) and (len(result) >= max_count):
-            break
-            
-    return result
-    
-def wait_element(self, context, attributes, timeout=30):
-    """
-    @arg timeout -1 means infinite
-    """
-    
-    result = []
-    while True:
-        print('Waitting element : %s.' % (str(attributes)))
-        
-        element = self.find_elements_by_attr(context, attributes, 1)
-        if element is not None:
-            return element
-            
-        time.sleep(1)
-        if timeout > 0:
-            timeout -= 1
-            if timeout <= 0:
-                break
-        
-    return None
 
 def force_focus(self):
     global_selenium.webdriver.ActionChains(self._parent).move_to_element(self).perform()
 
 def force_click(self):
-    global_selenium.webdriver.ActionChains(self._parent).move_to_element(self).click(self).perform()
+    self._parent.execute_script("arguments[0].click();", self);
         
