a3d6dd84c67abaa6f1629a8328f39f6fc3af6e0b
Hong-She Liang <starofrainnight@gmail.com>
2014-12-16 09:43:16 +0000

    Fixed wrong method type be set to WebElement and WebDriver.

--- automation/src/rabird/automation/selenium/__init__.py
+++ automation/src/rabird/automation/selenium/__init__.py
@@ -5,11 +5,12 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.command import Command 
 from selenium.webdriver.remote.webdriver import WebDriver
 from . import webelement 
+import types
 
 def monkey_patch():
-    WebElement.set_attribute = webelement.set_attribute
-    WebElement.force_focus = webelement.force_focus    
-    WebElement.force_click = webelement.force_click
-    WebElement.wait_element = webelement.wait_element
-    WebDriver.wait_element = webelement.wait_element
+    WebElement.set_attribute = types.MethodType(webelement.set_attribute, None, WebElement) 
+    WebElement.force_focus = types.MethodType(webelement.force_focus, None, WebElement)    
+    WebElement.force_click = types.MethodType(webelement.force_click, None, WebElement)
+    WebElement.wait_element = types.MethodType(webelement.wait_element, None, WebElement)
+    WebDriver.wait_element = types.MethodType(webelement.wait_element, None, WebDriver)
     
