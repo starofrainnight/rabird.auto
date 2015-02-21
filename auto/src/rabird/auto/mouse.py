efc726b5819ee969863c66dba19018dedf5f901c
Hong-She Liang <starofrainnight@gmail.com>
2015-02-21 01:57:34 +0000

    Moved mouse constants to module mouse_constant .

--- auto/src/rabird/auto/mouse.py
+++ auto/src/rabird/auto/mouse.py
@@ -6,28 +6,7 @@ import win32api, win32con, win32gui
 import time
 import datetime
 import rabird.core.datetime
-from rabird.core.enum import Enum 
-
-# button types
-class ButtonType(Enum):
-	LEFT = 0
-	MIDDLE = 1
-	RIGHT = 2
-
-# button status
-class ButtonStatus(Enum):
-	UP = 0
-	DOWN = 1
-
-## all time related unit are second, see description about sleep() function 
-# of module time.
-class ButtonOptions():
-	click_delay = 0.010
-	click_down_delay = 0.010
-	click_drag_delay = 0.250
-
-## options of mouse related functions
-options = ButtonOptions()
+from .mouse_constant import *
 
 ##
 # send event to system 
