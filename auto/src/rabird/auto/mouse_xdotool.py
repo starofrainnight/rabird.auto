7cd3cc1a6a023a093c9c6d8df4dd1b9d6c871faa
Hong-She Liang <starofrainnight@gmail.com>
2015-02-22 12:29:32 +0000

    Convert all os.system() to subprocess.check_output().

--- auto/src/rabird/auto/mouse_xdotool.py
+++ auto/src/rabird/auto/mouse_xdotool.py
@@ -12,32 +12,31 @@ class Mouse(object):
     ## return current mouse absolute position
     @classmethod
     def position(cls):
-        p = subprocess.Popen(["xdotool", "getmouselocation"], stdout=subprocess.PIPE)
-        output = p.communicate()[0]
+        output = subprocess.check_output(["xdotool", "getmouselocation"])
         matched = re.match(".*x:(\d+)\s*y:(\d+)\s*.*", output)
         return [int(matched.group(1)), int(matched.group(2))]
     
     @classmethod
     def move(cls, position):
-        os.system("xdotool mousemove --sync %s %s" % (int(position[0]), int(position[1])))
+        subprocess.call(["xdotool", "mousemove", "--sync", position[0], position[1]])
     
     ##  
     @classmethod
     def button_up(cls, button_type = ButtonType.LEFT ):
         if ButtonType.LEFT == button_type:
-            os.system("xdotool mouseup 1") 
+            subprocess.call(["xdotool", "mouseup", "1"]) 
         elif ButtonType.RIGHT == button_type:
-            os.system("xdotool mouseup 3")
+            subprocess.call(["xdotool", "mouseup", "3"])
         elif ButtonType.MIDDLE == button_type:
-            os.system("xdotool mouseup 2")
+            subprocess.call(["xdotool", "mouseup", "2"])
             
     @classmethod
     def button_down(cls, button_type = ButtonType.LEFT ):
         if ButtonType.LEFT == button_type:
-            os.system("xdotool mousedown 1")
+            subprocess.call(["xdotool", "mousedown", "1"])
         elif ButtonType.RIGHT == button_type:
-            os.system("xdotool mousedown 3")
+            subprocess.call(["xdotool", "mousedown", "3"])
         elif ButtonType.MIDDLE == button_type:
-            os.system("xdotool mousedown 2")
+            subprocess.call(["xdotool", "mousedown", "2"])
             
             
