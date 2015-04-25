c29d0c6b587cff3e327ab1c44885433764d17866
Hong-She Liang <starofrainnight@gmail.com>
2015-04-25 10:13:34 +0800

    Follow the setup process changes of rabird.core
    Update ez_setup.py to 1.1.7
    Renamed LICENSE to LICENSE.txt
    Replaced Markdown format README by ReStructuredText format
    Follow the setup.py changes of rabird.core
    Version upgrade to 0.3.1

---
+++ README.rst
@@ -0,0 +1,20 @@
+rabird.core
+-----------
+
+The base library for rabird.\* series modules.
+
+This library provided a series helper utilties for production, they are
+disordered and grow up while other module needs, there are no plan and
+just for production practice:
+
+-  An advance ConfigParser
+-  C-style string manage functions
+-  Some CPU related datetime functions
+-  Distutils for preprocess source to compatilbe with python 2.7
+-  Html related stuffs
+-  Logging functions
+-  Some system related stuffs
+-  Windows fixes just like "fix console can't output unicode string in
+   python 2.7", etc .
+-  Miscellaneous ...
+
--- setup.py
+++ setup.py
@@ -10,20 +10,15 @@ import shutil
 import logging
 import fnmatch
 import rabird.core.distutils
-import rabird.core.logging
 from setuptools import setup, find_packages
 
-from_package = 'src'
-to_package = 'rabird'
 package_name = 'rabird.auto'
 
-rabird.core.logging.load_default_config()
-
 # Convert source to v2.x if we are using python 2.x.
-rabird.core.distutils.preprocess_sources_for_compatible(from_package, os.path.realpath(os.curdir))
+source_dir = rabird.core.distutils.preprocess_source()
 
 # Exclude the original source package, only accept the preprocessed package!
-our_packages = find_packages(exclude=[from_package, '%s.*' % (from_package)])
+our_packages = find_packages(where=source_dir)
 
 our_requires = [
 	'pyscreenshot',
@@ -39,24 +34,37 @@ our_requires = [
 if sys.platform == 'win32':
 	our_requires.append('rabird.winio')
 	
+long_description=(
+     open("README.rst", "r").read()
+     + "\n" +
+     open("CHANGES.rst", "r").read()
+     )
+
 setup(
-	name=package_name,
-	version='.'.join(map(str, (0, 3, 0))),
-	author='Hong-She Liang',
-	author_email='starofrainnight@gmail.com',
-	url='',
-	py_modules=[to_package],
-	description='%s utilities' % package_name,
-	classifiers=[
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 3',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Topic :: Software Development :: Libraries',
-		'Topic :: Utilities',
-	],
-	install_requires=our_requires,
+    name=package_name,
+    version='.'.join(map(str, (0, 3, 1))),
+    author="Hong-She Liang",
+    author_email="starofrainnight@gmail.com",
+    url="https://github.com/starofrainnight/%s" % package_name,
+    description="The cross-platform windows automate library",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",        
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent", 
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",        
+        "Framework :: Rabird",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules", 
+    ],
+    install_requires=our_requires,
+    package_dir = {"": source_dir},
     packages=our_packages,
-    namespace_packages = ['rabird'],
-	)
+    namespace_packages=[package_name.split(".")[0]],
+    # If we don"t set the zip_safe to False, pip can"t find us.
+    zip_safe=False,
+    )
 
