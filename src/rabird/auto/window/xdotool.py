696ae5b7a6c29930a6fb972a7aa0e515715fd301
Hong-She Liang <starofrainnight@gmail.com>
2016-05-21 14:52:21 +0800

    Removed unused print codes

--- src/rabird/auto/window/xdotool.py
+++ src/rabird/auto/window/xdotool.py
@@ -14,23 +14,23 @@ def _check_output(*args, **kwargs):
     output = subprocess.check_output(*args, **kwargs)
     if six.PY3:
         output = output.decode(sys.getdefaultencoding())
-    return output 
-    
+    return output
+
 class Window(common.Window):
     def __init__(self, handle):
         super(Window, self).__init__()
-        
+
         self.__handle = int(handle)
-    
+
     @property
     def handle(self):
         return self.__handle
-        
+
     @property
     def title(self):
         output = _check_output(["xdotool", "getwindowname", str(self.__handle)])
         return output.strip('\r\n').strip()
-    
+
     @property
     def geometry(self):
         output = _check_output(["xdotool", "getwindowgeometry", str(self.__handle)])
@@ -46,35 +46,34 @@ class Window(common.Window):
                 "(?:\n|.)*"
                 "osition:[^\d]*(\d+),(\d+)[^\d]*(?:\n|.)*"
                 "eometry:[^\d]*(\d+)x(\d+)[^\d]*(?:\n|.)*", output, re.M)
-            print(matched)
-            
+
         return (int(matched.group(1)),
             int(matched.group(2)),
             int(matched.group(3)),
             int(matched.group(4)))
-    
+
     def raise_(self):
-        subprocess.call(["xdotool", "windowraise", "--sync", str(self.__handle)]) 
-        
+        subprocess.call(["xdotool", "windowraise", "--sync", str(self.__handle)])
+
     def activate(self):
         subprocess.call(["xdotool", "windowactivate", "--sync", str(self.__handle)])
-        
+
     def close(self):
         subprocess.call(["xdotool", "windowkill", str(self.__handle)])
-        
+
 class Manager(common.Manager):
-    
+
     def __init__(self):
         super(Manager, self).__init__()
         self.__mouse = Mouse()
-    
+
     def get_active(self):
         output = _check_output(["xdotool", "getactivewindow"])
         return Window(int(output.strip('\r\n').strip()))
-    
+
     def get_from_position(self, position):
         old_position = None
-        
+
         if position != self.__mouse.position():
             old_position = self.__mouse.position()
             self.__mouse.move(position)
@@ -84,42 +83,41 @@ class Manager(common.Manager):
             return Window(int(matched.group(1)))
         finally:
             if old_position is not None:
-                self.__mouse.move(old_position)        
-    
+                self.__mouse.move(old_position)
+
     def find(self, **kwargs):
         self._prepare_find_arguments(kwargs)
-        
+
         result = []
-        
+
         command = ["xdotool", "search"]
         command.append("--all")
         # It will freeze the search behaviors
-        # command.append("--sync") 
-        
-        # Seems xdotool can't work if class name and title 
+        # command.append("--sync")
+
+        # Seems xdotool can't work if class name and title
         # at the sametime. So we search by ourself.
-        command += ["--name", ''] # Search all windows 
-        
+        command += ["--name", ''] # Search all windows
+
         output = _check_output(command)
         window_ids = re.findall("\d+", output, re.M)
         window_ids = [int(window_id) for window_id in window_ids]
-        
+
         for window_id in window_ids:
             window = Window(window_id)
-            
-            if (("title" in kwargs) and 
+
+            if (("title" in kwargs) and
                 (re.match(str(kwargs["title"]), window.title) is None)):
                 continue
-            
-            if (("class_name" in kwargs) and 
+
+            if (("class_name" in kwargs) and
                 (re.match(str(kwargs["class_name"]), window.class_name) is None)):
                 continue
 
             result.append(window)
-            
+
             if kwargs["limit"] > 0:
                 if len(result) >= kwargs["limit"]:
                     break
-        
+
         return result
-    
